72b87011000528413a67e18c19ee0544d7ef045a
Corinna <corinnaccm@gmail.com>
2024-01-12 21:40:29 +0000

    functions for assigning pairs and email text

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from privacy import sender
-from privacy import password
+from googleapiclient.discovery import build
+import base64
+import email.message
 import random
 import smtplib
 
@@ -7,7 +8,7 @@ class LunchBuddy:
 
     buddy1 = []
     buddy2 = []
-    pairs = {}
+    matched_pairs = {}
 
     
     def names_from_sheet(buddy1):
@@ -18,6 +19,7 @@ class LunchBuddy:
 
     def create_lists(buddy1):
         buddy2 = random.sample(buddy1, len(buddy1))
+        buddys_email
 
 #Run a check so nobody gets themselves as the lunch buddy
 
@@ -32,10 +34,13 @@ class LunchBuddy:
                 buddy2[0] = buddy2[len(buddy2)-1]
                 buddy2[len(buddy2)-1] = temp_var
 
-    def assign_pairs(buddy1):
-        pass
+    def assign_pairs(buddy1, buddy2):
+        matched_pairs = dict(zip(buddy1, buddy2))
 
-    def email_content(buddy1, buddy2):
+    def email_text(buddy1, buddy2):
+        
+        print("""Hi {b1}, your Lunch Buddy this week is {b2}. 
+              Be sure to email them to set up a lunch this week!""".format(b1=buddy1.title(), b2=buddy2.title()))    
 
     def send_emails(buddy1):
         pass
